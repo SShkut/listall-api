827ec9d65f7a67d5985a963e14957b4dc02e37d6
Sviatoslav Shkut <sgshkut@gmail.com>
2023-10-25 22:18:11 +0200

    Playing with serde

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,28 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+#[derive(serde::Deserialize)]
+struct ProductFormData {
+    name: String,
+}
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn create_product(_form: web::Form<ProductFormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/products", web::post().to(create_product))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -15,6 +15,40 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length())
 }
 
+#[tokio::test]
+async fn create_product_returns_200_for_valid_product_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=product";
+    let response = client
+        .post(format!("{}/products", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute requets");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn create_product_returns_400_when_data_is_missing() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let invalid_body = "";
+    let response = client
+        .post(format!("{}/products", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(invalid_body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(400, response.status().as_u16());
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
